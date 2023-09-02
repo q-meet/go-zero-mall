c00431b4ae688131ce5b783754a4595379dc55fe
玲珑 <q-meet>
2023-09-02 15:35:06 +0700

    日志迭代

--- userApi/userapi.go
+++ userApi/userapi.go
@@ -4,11 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"github.com/zeromicro/go-zero/core/logx"
-	"rpc-common/log/zapx"
-
 	"go-zero/mall/user/Api/internal/config"
 	"go-zero/mall/user/Api/internal/handler"
 	"go-zero/mall/user/Api/internal/svc"
+	"rpc-common/log/zapx"
 
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/rest"
@@ -25,12 +24,12 @@ func main() {
 	server := rest.MustNewServer(c.RestConf)
 	defer server.Stop()
 
+	ctx := svc.NewServiceContext(c)
+	handler.RegisterHandlers(server, ctx)
+
 	writer, err := zapx.InitLogger()
 	logx.Must(err)
 	logx.SetWriter(writer)
-
-	ctx := svc.NewServiceContext(c)
-	handler.RegisterHandlers(server, ctx)
 
 	logx.Infof("Starting server at %s:%d...\n", c.Host, c.Port)
 	logx.Infow(fmt.Sprintf("Starting server at %s:%d...\n", c.Host, c.Port), logx.Field("host", c.Host))
